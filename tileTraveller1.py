95d988075975af83bda271e14fa0c416f3fedf7e
sjunia <sigrunjunia@gmail.com>
2018-09-18 10:28:43 +0000

    bjó til algorithm fyrir TileTraveller sem comment

---
+++ tileTraveller1.py
@@ -0,0 +1,8 @@
+#Algorithm for project TileTraveller
+#1. Program prints valid directions to move from current tile
+#2. If player choses not valid direction program prints on screen Not valid direction!
+#3. If player choses vaild move he moves to the next tile
+#4. When player reaches final tile 3.1 he wins the game
+#5. Program goes back to line 1 if not on final tile
+
+
