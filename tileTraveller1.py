697af5f373cb6bc5362b3dc103d9147ec71f8b35
sjunia <sigrunjunia@gmail.com>
2018-09-19 12:38:06 +0000

    Sett inni github slóð í verkefnið.

--- tileTraveller1.py
+++ tileTraveller1.py
@@ -1,9 +1,12 @@
+#GITHUB slóð https://github.com/sjunia/TileTraveller.git
+
 #Algorithm for project TileTraveller
 #1. Program prints valid directions to move from current tile
 #2. If player choses not valid direction program prints on screen Not valid direction!
 #3. If player choses vaild move he moves to the next tile
 #4. When player reaches final tile 3.1 he wins the game
 #5. Program goes back to line 1 if not on final tile
+
 
 
 current_x = 1
@@ -68,7 +71,7 @@ while win is False:
 
     #1.3
     if current_x == 1 and current_y == 3:
-        print("You can travel: (E)ast or (S)outh")
+        print("You can travel: (E)ast or (S)outh.")
         selection = input("Direction: ")
         if selection is "e" or selection is "E":
             current_x = 2
@@ -79,14 +82,14 @@ while win is False:
             current_y = 2
             continue
         else:
-            while selection is not "e" or selection is not "E":
+            while selection is not "e" or selection is not "E" or selection is not "s" or selection is not "S":
                 print("Not a valid direction!")
                 selection = input("Direction: ")
-                if selection is not "e" or selection is not "E":
+                if selection is "e" or selection is "E":
                     current_x = 2
                     current_y = 3
                     break
-                if selection is not "s" or selection is not "S":
+                elif selection is "s" or selection is "S":
                     current_x = 1
                     current_y = 2
                     break
@@ -94,20 +97,25 @@ while win is False:
 
     #2.1
     if current_x == 2 and current_y == 1:
-        print("You can travel: (N)orth")
+        print("You can travel: (N)orth.")
         selection = input("Direction: ")
         if selection is "n" or selection is "N":
             current_x = 2
             current_y = 2
             continue
         else:
-            print("Not a valid direction!")
-            selection = input("Direction: ")
+            while selection is not "n" or selection is not "N":
+                print("Not a valid direction!")
+                selection = input("Direction: ")
+                if selection is "n" or selection is "N":
+                    current_x = 2
+                    current_y = 2
+                    break
             	
     
     #2.2
     if current_x == 2 and current_y == 2:
-        print("You can travel: (S)outh or (W)est")
+        print("You can travel: (S)outh or (W)est.")
         selection = input("Direction: ")
         if selection is "s" or selection is "S":
             current_x = 2
@@ -118,11 +126,20 @@ while win is False:
             current_y = 2
             continue
         else:
-            print("Not a valid direction!")
-            selection = input("Direction: ")
+            while selection is not "s" or selection is not "S" or selection is not "w" or selection is not "W":
+                print("Not a valid direction!")
+                selection = input("Direction: ")
+                if selection is "s" or selection is "S":
+                    current_x = 2
+                    current_y = 1
+                    break   
+                elif selection is "w" or selection is "W":
+                    current_x = 1
+                    current_y = 2
+                    break
     #2.3
     if current_x == 2 and current_y == 3:
-        print("You can travel: (E)ast or (W)est")
+        print("You can travel: (E)ast or (W)est.")
         selection = input("Direction: ")
         if selection is "e" or selection is "E":
             current_x=3
@@ -133,25 +150,45 @@ while win is False:
             current_y=3
             continue
         else:
-            print("Not a valid direction!")
-            selection = input("Direction: ")
+            while selection is not "e" or selection is not "E" or selection is not "w" or selection is not "W":
+                print("Not a valid direction!")
+                selection = input("Direction: ")
+                if selection is "e" or selection is "E":
+                    current_x=3
+                    current_y=3
+                    break
+                elif selection is "w" or selection is "W":
+                    current_x=1
+                    current_y=3
+                    break
 
     #3.2
     if current_x == 3 and current_y == 2:
-        print("You can travel: (N)orth or (S)out")
+        print("You can travel: (N)orth or (S)out.")
         selection = input("Direction: ")
         if selection is "n" and selection is "N":
             current_x=3
             current_y=3
+            continue
         if selection is "s" or selection is "S":
             current_x=3
             current_y=1
+            continue
         else:
-            print("Not a valid direction!")
-            selection = input("Direction: ")
+            while selection is not "n" or selection is not "N" or selection is not "s" or selection is not "S":
+                print("Not a valid direction!")
+                selection = input("Direction: ")
+                if selection is "n" and selection is "N":
+                    current_x=3
+                    current_y=3
+                    break
+                elif selection is "s" or selection is "S":
+                    current_x=3
+                    current_y=1
+                    break
     #3.3
     if current_x == 3 and current_y == 3:
-        print("You can travel: (S)outh or (W)est")
+        print("You can travel: (S)outh or (W)est.")
         selection = input("Direction: ")
         if selection is "s" or selection is "S":
             current_x=3
@@ -163,8 +200,18 @@ while win is False:
             continue
 
         else:
-            print("Not a valid direction!")
-            selection = input("Direction: ")
+            while selection is not "s" or selection is not "S" or selection is not "w" or selection is not "W":
+                print("Not a valid direction!")
+                selection = input("Direction: ")
+                if selection is "s" or selection is "S":
+                    current_x=3
+                    current_y=2
+                    break
+                elif selection is "w" or selection is "W":
+                    current_x=2
+                    current_y=3
+                    break
+            
     
     #3.1
     if current_x == 3 and current_y == 1:
@@ -172,7 +219,7 @@ while win is False:
         break 
  
 
-print("You won")
+print("Victory!")
 
 
         
